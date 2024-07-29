4ead4e1770606fdfe24886ba97c30cec574290c5
SirCartier <akmalkhairy90@gmail.com>
2024-07-29 13:05:44 +0800

    Create products array

---
+++ data/product.js
@@ -0,0 +1,158 @@
+const products = [
+  {
+    id: '1',
+    name: 'Feline Combat Glasses',
+    image: 'images/product/feline-combat-glasses.png',
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 4900,
+    keywords: [
+      'glasses',
+      'visors',
+      'goggles'
+    ]
+  }, {
+    id: '2',
+    name: 'Tactical Scratching Post',
+    image: 'images/product/tactical-scratching-post.png',
+    rating: {
+      stars: 5,
+      count: 127
+    },
+    priceCents: 5900,
+    keywords: [
+      'scratching',
+      'post'
+    ]
+  }, {
+    id: '3',
+    name: 'Kitty Kevlar Cap',
+    image: 'images/product/kitty-kevlar-cap.png',
+    rating: {
+      stars: 3.5,
+      count: 56
+    },
+    priceCents: 8900,
+    keywords: [
+      'kevlar',
+      'cap',
+      'helmet',
+      'bulletproof',
+      'safety'
+    ]
+  }, {
+    id: '4',
+    name: 'Meowtary Grenade Harness',
+    image: 'images/product/meowtary-granade-harness.png',
+    rating: {
+      stars: 4.5,
+      count: 175
+    },
+    priceCents: 6900,
+    keywords: [
+      'grenade',
+      'harness',
+    ]
+  }, {
+    id: '5',
+    name: 'Cat-Eye Goggles',
+    image: 'images/product/cat-eye-goggles.png',
+    rating: {
+      stars: 4,
+      count: 317
+    },
+    priceCents: 9900,
+    keywords: [
+      'glasses',
+      'visors',
+      'goggles'
+    ]
+  }, {
+    id: '6',
+    name: 'Clawed Camo Vest',
+    image: 'images/product/clawed-camo-vest.png',
+    rating: {
+      stars: 3,
+      count: 305
+    },
+    priceCents: 4900,
+    keywords: [
+      'vest',
+      'kevlar',
+      'bulletproof',
+      'safety'
+    ]
+  }, {
+    id: '7',
+    name: 'Purrtection Helmet',
+    image: 'images/product/purrtection-helmet.png',
+    rating: {
+      stars: 4.5,
+      count: 89
+    },
+    priceCents: 4900,
+    keywords: [
+      'kevlar',
+      'cap',
+      'helmet',
+      'bulletproof',
+      'safety'
+    ]
+  }, {
+    id: '8',
+    name: 'Tactical Climbing Harnesses',
+    image: 'images/product/tactical-climbing-harnesses.png',
+    rating: {
+      stars: 5,
+      count: 70
+    },
+    priceCents: 7900,
+    keywords: [
+      'harness',
+      'vest',
+      'safety'
+    ]
+  }, {
+    id: '9',
+    name: 'Purrtection Camo Gear',
+    image: 'images/product/purrfect-camo-gear.png',
+    rating: {
+      stars: 3.5,
+      count: 81
+    },
+    priceCents: 3900,
+    keywords: [
+      'camo',
+      'vest',
+      'safety'
+    ]
+  }, {
+    id: '10',
+    name: 'Whisker Warrior Gloves',
+    image: 'images/product/whisker-warrior-gloves.png',
+    rating: {
+      stars: 4.5,
+      count: 35
+    },
+    priceCents: 5900,
+    keywords: [
+      'gloves'
+    ]
+  }, {
+    id: '11',
+    name: 'Scratch Sniper Rifle',
+    image: 'images/product/scratch-sniper-rifle.png',
+    rating: {
+      stars: 1,
+      count: 99
+    },
+    priceCents: 299900,
+    keywords: [
+      'weapon',
+      'sniper',
+      'rifle'
+    ]
+  }
+];
