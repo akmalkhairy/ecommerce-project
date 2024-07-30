ed9c999d8b320deb24f2a53da7803f6df0453a93
SirCartier <akmalkhairy90@gmail.com>
2024-07-30 16:01:01 +0800

    Create deliveryOptions array

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions = [{
+  id: '1',
+  priceCents: 'FREE',
+  deliveryDays: '7'
+}, {
+  id: '2',
+  priceCents: '499',
+  deliveryDays: '3'
+}, {
+  id: '3',
+  priceCents: '999',
+  deliveryDays: '1'
+}];
